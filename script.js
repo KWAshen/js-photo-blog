ced064f18244abb2c983aab8668822f79751cb4f
KWAshen <ashen.maliaha@gmail.com>
2025-07-22 21:44:49 +0200

    ultime modifiche

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const overlay = document.getElementById('overlay');
   const closeBtn = document.getElementById('closeBtn');
-
+  const openPopUp = document.getElementById('openPopUp');
  
   closeBtn.addEventListener('click', () => {
     overlay.style.display = 'none';
   });
 });
+
+
+  openPopUp.addEventListener('click', () => {
+    overlay.style.display = 'flex';
+  });
+
+ 
+  // Mostra l'overlay al caricamento iniziale
+  overlay.style.display = 'flex';
